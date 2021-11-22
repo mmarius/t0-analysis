c0836274df0137d0e185c8ea9fd82a1a61c2853a
Marius Mosbach <m_marius@online.de>
2021-11-22 18:10:30 +0100

    fixed script

--- examples/model_ parallelism_hf.py
+++ examples/model_ parallelism_hf.py
@@ -1,52 +1,22 @@
 # Copied from https://gist.github.com/VictorSanh/049387b8dc0e6ac8f83ba55feb04917f
-# Adapted from https://huggingface.co/transformers/main_classes/deepspeed.html#non-trainer-deepspeed-integration
-
+# Model parallelism
+# Tested: 4 16GB V100 or 2 32GB V100 (basically need somewhere to fit ~42GB of fp32 params)
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from transformers.deepspeed import HfDeepSpeedConfig
-import deepspeed
-import os
 import torch
-
-# To avoid warnings about parallelism in tokenizers
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 model_name = "bigscience/T0"
 
-ds_config = {
-    "fp16": {
-        "enabled": False,
-    },
-    "zero_optimization": {
-        "stage": 3,
-        "offload_param": {
-            "device": "cpu",
-            "pin_memory": True
-        },
-        # To tune. Current value was set to max out a 40GB A100 GPU.
-        "stage3_param_persistence_threshold": 4e7,
-    },
-    "train_batch_size": 1,
-}
-
-_ = HfDeepSpeedConfig(ds_config)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 print("Model and tokenizer loaded")
 
-inputs = tokenizer.encode(
-    "Review: this is the best cast iron skillet you will ever buy. Is this review positive or negative?", return_tensors="pt")
+model.parallelize()
+print("Moved model to GPUs")
+
+inputs = tokenizer.encode("Review: this is the best cast iron skillet you will ever buy. Is this review positive or negative?", return_tensors="pt")
 inputs = inputs.to("cuda:0")
-
-deepspeed_engine, _, _, _ = deepspeed.initialize(
-    model=model,
-    config_params=ds_config,
-    model_parameters=None,
-    optimizer=None,
-    lr_scheduler=None
-)
-
-deepspeed_engine.module.eval()
 with torch.no_grad():
-    outputs = deepspeed_engine.module.generate(inputs)
+    outputs = model.generate(inputs)
+
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 print("FINISHED")
